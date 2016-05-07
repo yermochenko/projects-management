8a63d42dcd371b23a0eeb7736aee7928b1eca584
beriks4 <beriks4@gmail.com>
2016-05-07 11:27:04 +0300

    Заполнение таблиц employee, contact, contacts_type

--- sql/05_fill.sql
+++ sql/05_fill.sql
@@ -45,6 +45,11 @@ INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (8, 1, 1);
 INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (9, 1, 8);
 INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (10, 1, 7);
 
+#Записи в таблице "contact_type"
+INSERT INTO `contacts_type` (`id`, `name`, `regexp`) VALUES (1, 'email', '^[a-zA-Z]([.]?([a-zA-Z0-9_-]+)*)?@([a-zA-Z0-9\\-_]+\\.)+[a-zA-Z]{2,4}$');
+INSERT INTO `contacts_type` (`id`, `name`, `regexp`) VALUES (2, 'telephone', '^([+]375)[0-9]{9}');
+INSERT INTO `contacts_type` (`id`, `name`, `regexp`) VALUES (3, 'addres', '');
+
 #Записи в таблице "contact"
 INSERT INTO `contact` (`id`, `name`, `user_id`, `type_id`) VALUES (1, '+375555666', 1, 2);
 INSERT INTO `contact` (`id`, `name`, `user_id`, `type_id`) VALUES (2, 'Actor@mail.com', 7, 1);
@@ -56,11 +61,6 @@ INSERT INTO `contact` (`id`, `name`, `user_id`, `type_id`) VALUES (7, '+37533325
 INSERT INTO `contact` (`id`, `name`, `user_id`, `type_id`) VALUES (8, 'Moskovskyi pr.', 3, 3);
 INSERT INTO `contact` (`id`, `name`, `user_id`, `type_id`) VALUES (9, 'timka@mail.com', 2, 1);
 INSERT INTO `contact` (`id`, `name`, `user_id`, `type_id`) VALUES (10, '+3752564568', 2, 2);
-
-#Записи в таблице "contact_type"
-INSERT INTO `contacts_type` (`id`, `name`, `regexp`) VALUES (1, 'email', '^[a-zA-Z]([.]?([a-zA-Z0-9_-]+)*)?@([a-zA-Z0-9\\-_]+\\.)+[a-zA-Z]{2,4}$');
-INSERT INTO `contacts_type` (`id`, `name`, `regexp`) VALUES (2, 'telephone', '^([+]375)[0-9]{9}');
-INSERT INTO `contacts_type` (`id`, `name`, `regexp`) VALUES (3, 'addres', '');
 
 #Записи в таблице "employee"
 INSERT INTO `employee` (`id`, `user_id`, `team_id`, `role`) VALUES (1, 7, 1, 0);
