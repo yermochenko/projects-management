56e4e6b4f1334b296947519d782c6872250bbeb2
Rogovoy <rogocop11@gmail.com>
2016-06-03 17:06:10 +0300

    Заполнение таблиц  БД тестовыми данными (project, project_category)

--- sql/11_fill.sql
+++ sql/11_fill.sql
@@ -1,19 +1,3 @@
-#Записи в таблице project
-INSERT INTO `project` (`id`,  `name`, `description`, `category_id`, `manager_id`) VALUES (1, 'VSU', NULL );
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (2, 'The Faculty of Mathematics', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (3, 'The Faculty of Law', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (4, 'The Faculty of Art and Graphics', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (5, 'The Faculty of Physical Culture and Sports', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (6, 'The Faculty of Social Pedagogy and Psychology', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (7, 'The Faculty of Pedagogy', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (8, 'The Faculty of Biology', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (9, 'The Faculty of History', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (10, 'The Faculty of Preliminary Training', 1);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (11, 'Group 41', 2);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (12, 'Group 43', 2);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`, `category_id`, `manager_id`) VALUES (13, 'Subgroup_1 (41)', 11);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (14, 'Subgroup_2 (41)', 11);
-INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (15, 'Applied Mathematics and Mechanics', 2);
 #Записи в таблице project_category
 INSERT INTO `projects_category` (`id`, `name`, `parent_id`) VALUES (1, 'Test category', 1);
 INSERT INTO `projects_category` (`id`, `name`, `parent_id`) VALUES (2, 'University category', 2);
@@ -25,3 +9,14 @@ INSERT INTO `projects_category` (`id`, `name`, `parent_id`) VALUES (7, 'Medicine
 INSERT INTO `projects_category` (`id`, `name`, `parent_id`) VALUES (8, 'Medicine( pharmacy) category', 7);
 INSERT INTO `projects_category` (`id`, `name`, `parent_id`) VALUES (9, 'Medicine( Emergency Room) category', 7);
 INSERT INTO `projects_category` (`id`, `name`, `parent_id`) VALUES (10, 'Web technology category', 10);
+#Записи в таблице project
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (1, 'test', '\r\ntest data', 1, 5);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (4, 'Statement of the educational process', 'Timetable of classes ,tests, exams, retake', 2, 10);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (7, 'Аmbulance project', 'Сustomer requirements', 9, 8);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (14, 'Banking system', 'Castumer requirements "Belinvestbank"', 6, 3);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (15, 'Banking system', 'Custumer requirements "Belarusbank" ', 6, 2);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (16, 'www.Onliner.by', 'Styling website', 10, 6);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (18, 'www.Plus-Minus.by', 'Search for bugs and errors', 10, 9);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (19, 'IOS', 'Software development', 5, 4);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (22, 'ANDROID', 'Software development', 5, 6);
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (24, 'Shporgalki.vsu', 'help students', 10, 8);
