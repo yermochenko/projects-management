00d8ca2b9bb85185147e42c0709401105e1ed65c
Sergey Yermochenko <yermochenko@gmail.com>
2016-06-06 23:26:59 +0300

    При генерации тестовых данных добавлен новый проект для исключения конфликтов с другими скриптами. Для таблицы actor добавлены данные для поля is_abstract. В тестовых данных все данные изменены на более-менее правдоподобные

--- sql/10_fill.sql
+++ sql/10_fill.sql
@@ -1,23 +1,23 @@
+INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`) VALUES (2, 'project', 'there is need to complete term paper', 1, 4);
+
 --Записи в таблице actor
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (1, 'denis', 1);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (2, 'vlad', 4);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (3, 'vova', 7);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (4, 'nik', 14);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (5, 'pavel', 15);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (6, 'boris', 16);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (7, 'ann', 18);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (8, 'den', 19);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (9, 'klava', 22);
-INSERT INTO `actor` (`id`, `name`, `project_id`) VALUES (10, 'petya', 24);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (3, 'unauthorized user', 2, FALSE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (4, 'authorised user', 2, TRUE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (5, 'client', 2, FALSE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (6, 'VIP client', 2, FALSE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (7, 'employee', 2, TRUE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (8, 'courier', 2, FALSE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (9, 'manager', 2, TRUE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (10, 'product manager', 2, FALSE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (11, 'sales manager', 2, FALSE);
+INSERT INTO `actor` (`id`, `name`, `project_id`, `is_abstract`) VALUES (12, 'admin', 2, FALSE);
 
 --Записи в таблице actors_relation
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (1, 1, 1);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (2, 2, 1);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (3, 3, 2);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (4, 4, 3);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (5, 5, 4);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (6, 6, 5);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (7, 7, 6);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (8, 8, 8);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (9, 9, 7);
-INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (10, 10, 2);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (1, 4, 5);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (2, 4, 7);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (3, 5, 6);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (4, 7, 8);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (5, 7, 9);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (6, 7, 12);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (7, 9, 10);
+INSERT INTO `actors_relation` (`id`, `parent_id`, `child_id`) VALUES (8, 9, 11);
