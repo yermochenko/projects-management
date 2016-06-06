12eba7dad4f4d76504b1d502fd84ca5cb6cfd1dc
belka2594 <belka2594@gmail.com>
2016-06-06 15:59:07 +0300

    added Записи в таблице "use_case_relation"

--- sql/09_fill.sql
+++ sql/09_fill.sql
@@ -9,4 +9,7 @@ INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (7, 'Plani
 INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (8, 'Stereometry', 6, 4);
 INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (9, 'Projective geometry', 6, 4);
 
-
+--Записи в таблице "use_case_relation"
+INSERT INTO `use_case_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (1, 1, 2 ,0);
+INSERT INTO `use_case_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (2, 2, 1 ,2);
+INSERT INTO `use_case_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (3, 3, 3 ,1);
