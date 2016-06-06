c0e21138e9829a9caf8eb657e597e53cb14f1a0b
Sergey Yermochenko <yermochenko@gmail.com>
2016-06-06 23:13:23 +0300

    В тестовых даных убраны двойные назначения ведущих программистов в одной и той же команде

--- sql/05_fill.sql
+++ sql/05_fill.sql
@@ -37,10 +37,8 @@ INSERT INTO `project` (`id`, `name`, `description`, `category_id`, `manager_id`)
 INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (1, 1, 4);
 INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (2, 1, 5);
 INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (3, 1, 3);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (4, 1, 4);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (5, 1, 2);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (6, 1, 4);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (7, 1, 10);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (8, 1, 1);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (9, 1, 8);
-INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (10, 1, 7);
+INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (4, 1, 2);
+INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (5, 1, 10);
+INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (6, 1, 1);
+INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (7, 1, 8);
+INSERT INTO `team` (`id`, `project_id`, `leader_id`) VALUES (8, 1, 7);
