737378549dc2c60c2e8f3b162b05cd5137b1e435
vladlialiuha <vladlialiuha@gmail.com>
2016-06-06 15:45:30 +0200

    Заполнение таблиц "use_case" и "actor_use_case_relation"

---
+++ sql/08_fill.sql
@@ -0,0 +1,27 @@
+#Записи в таблице "actor_use_case_relation"
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (1, 1, 1);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (2, 2, 2);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (3, 3, 3);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (4, 4, 4);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (5, 5, 5);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (6, 6, 6);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (7, 7, 7);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (8, 8, 8);
+INSERT INTO `actor_use_case_relation` (`id`, `actor_id`, `use_case_id`) VALUES (9, 9, 9);
+
+
+
+
+
+#Записи в таблице "use_case"
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (1, 'test1', 1);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (2, 'test2', 2);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (3, 'test3', 3);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (4, 'test4', 4);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (5, 'test5', 5);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (6, 'test6', 6);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (7, 'test7', 7);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (8, 'test8', 8);
+INSERT INTO `use_case` (`id`, `name`, `module_id`) VALUES (9, 'test9', 9);
+
+
