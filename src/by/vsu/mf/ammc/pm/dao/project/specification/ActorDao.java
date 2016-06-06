f3f4971f0d77a1a468f69f4ac8633680d7bda3de
denvoropaev94 <denvoropaev7@gmail.com>
2016-06-06 14:31:34 +0400

    Заполнение таблиц БД(actor,actors_relation)

--- src/by/vsu/mf/ammc/pm/dao/project/specification/ActorDao.java
+++ src/by/vsu/mf/ammc/pm/dao/project/specification/ActorDao.java
@@ -3,4 +3,4 @@ package by.vsu.mf.ammc.pm.dao.project.specification;
 import by.vsu.mf.ammc.pm.dao.Dao;
 import by.vsu.mf.ammc.pm.domain.project.specification.Actor;
 
-public interface ActorDao extends Dao<Integer, Actor> {}
+public interface ActorDao extends , Actor> {}
