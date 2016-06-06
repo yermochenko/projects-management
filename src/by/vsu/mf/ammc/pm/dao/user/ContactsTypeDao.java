c1fdb146e89cac8f554622bf3c8ba1273c5ec298
Sergey Yermochenko <yermochenko@gmail.com>
2016-06-06 16:31:53 +0300

    DAO - добавлена возможность получения списка всех типов контактов

--- src/by/vsu/mf/ammc/pm/dao/user/ContactsTypeDao.java
+++ src/by/vsu/mf/ammc/pm/dao/user/ContactsTypeDao.java
@@ -1,6 +1,11 @@
 package by.vsu.mf.ammc.pm.dao.user;
 
+import java.util.List;
+
 import by.vsu.mf.ammc.pm.dao.Dao;
 import by.vsu.mf.ammc.pm.domain.user.ContactsType;
+import by.vsu.mf.ammc.pm.exception.DaoException;
 
-public interface ContactsTypeDao extends Dao<Integer, ContactsType> {}
+public interface ContactsTypeDao extends Dao<Integer, ContactsType> {
+	List<ContactsType> read() throws DaoException;
+}
