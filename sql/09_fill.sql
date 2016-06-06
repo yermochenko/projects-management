11fe71e29eba04c00e8cada8513c2a59faa57875
Sergey Yermochenko <yermochenko@gmail.com>
2016-06-06 23:22:02 +0300

    При генерации тестовых данных исправлена ошибка в имени таблицы use_cases_relation. Идентификаторы модулей сдвинуты на 2 для устранения конфликтов с другими скриптами. В тестовых данных в таблице use_cases_relation данные изменены на более-менее правдоподобные

--- sql/09_fill.sql
+++ sql/09_fill.sql
@@ -1,15 +1,16 @@
 --Записи в таблице "module"
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (1, 'MATHEMATICS', NULL, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (2, 'physics', NULL, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (3, 'Russian', NULL, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (4, 'mathematical analysis', 1, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (5, 'differential equations', 1, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (6, 'geometry', 1, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (7, 'Planimetry', 6, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (8, 'Stereometry', 6, 4);
-INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (9, 'Projective geometry', 6, 4);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (3, 'MATHEMATICS', NULL, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (4, 'physics', NULL, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (5, 'Russian', NULL, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (6, 'mathematical analysis', 3, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (7, 'differential equations', 3, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (8, 'geometry', 3, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (9, 'Planimetry', 8, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (10, 'Stereometry', 8, 1);
+INSERT INTO `module` (`id`, `name`, `parent_id`, `project_id`) VALUES (11, 'Projective geometry', 8, 1);
 
---Записи в таблице "use_case_relation"
-INSERT INTO `use_case_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (1, 1, 2 ,0);
-INSERT INTO `use_case_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (2, 2, 1 ,2);
-INSERT INTO `use_case_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (3, 3, 3 ,1);
+--Записи в таблице "use_cases_relation"
+INSERT INTO `use_cases_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (1, 7, 5, 2);
+INSERT INTO `use_cases_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (2, 7, 6, 2);
+INSERT INTO `use_cases_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (3, 8, 3, 1);
+INSERT INTO `use_cases_relation` (`id`, `source_id`, `destination_id`, `type` ) VALUES (4, 2, 9, 0);
