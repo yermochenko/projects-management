a5fc66aedbc984d3b092eac5524dda7ec4b813f6
belka2594 <belka2594@gmail.com>
2016-06-09 20:25:19 +0300

    Исправлена ошибка в сущности

--- src/by/vsu/mf/ammc/pm/domain/project/management/TasksCategory.java
+++ src/by/vsu/mf/ammc/pm/domain/project/management/TasksCategory.java
@@ -1,27 +1,5 @@
 package by.vsu.mf.ammc.pm.domain.project.management;
 
 import by.vsu.mf.ammc.pm.domain.HierarchyEntity;
-import javafx.scene.Parent;
 
-import javax.lang.model.element.Name;
-
-public class TasksCategory extends HierarchyEntity<TasksCategory> {
-    private Name name;
-    private Parent parent;
-
-    public Name getUser() {
-        return name;
-    }
-
-    public void setUser(Name name) {
-        this.name = name;
-    }
-
-    public Parent getTeam() {
-        return parent;
-    }
-
-    public void setTeam(Parent parent) {
-        this.parent = parent;
-    }
-}
+public class TasksCategory extends HierarchyEntity<TasksCategory> {}
