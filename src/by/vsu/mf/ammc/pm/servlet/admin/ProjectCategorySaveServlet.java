01307e7d8deeeeb46e9809fc5479b586e2cac467
Sergey Yermochenko <yermochenko@gmail.com>
2016-06-24 13:23:59 +0300

    Реализовано сохранение (создание и редактирование) категории проектов

--- src/by/vsu/mf/ammc/pm/servlet/admin/ProjectCategorySaveServlet.java
+++ src/by/vsu/mf/ammc/pm/servlet/admin/ProjectCategorySaveServlet.java
@@ -1,8 +1,9 @@
-package by.vsu.mf.ammc.pm.servlet.project_category;
+package by.vsu.mf.ammc.pm.servlet.admin;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,34 +13,32 @@ import by.vsu.mf.ammc.pm.exception.ServiceException;
 import by.vsu.mf.ammc.pm.service.ServiceLocator;
 import by.vsu.mf.ammc.pm.service.project.ProjectsCategoryService;
 
-/**
- * Created by likan on 10.06.2016.
- */
+@WebServlet("/admin/project/category/save.html")
 public class ProjectCategorySaveServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
 		Integer id = null;
 		String name = req.getParameter("name");
-		Integer parent_id = null;
+		Integer parentId = null;
 		try {
 			id = Integer.parseInt(req.getParameter("id"));
 		} catch(NumberFormatException e) {}
 		try {
-			parent_id = Integer.parseInt(req.getParameter("parent_id"));
+			parentId = Integer.parseInt(req.getParameter("parent"));
 		} catch(NumberFormatException e) {}
 		if(name != null) {
-			ProjectsCategory type = new ProjectsCategory();
-			type.setId(id);
-			type.setName(name);
-			// type.setParent(UsersGroupService.findById(parent_id));
+			ProjectsCategory category = new ProjectsCategory();
+			category.setId(id);
+			category.setName(name);
 			ServiceLocator locator = null;
 			try {
 				locator = new ServiceLocator();
 				ProjectsCategoryService service = locator.getService(ProjectsCategoryService.class);
-				if(parent_id != null) {
-					type.setParent(service.findById(parent_id));
+				if(parentId != null) {
+					category.setParent(service.findById(parentId));
 				}
-				service.save(type);
+				service.save(category);
 			} catch(ServiceException e) {
 				throw new ServletException(e);
 			} finally {
@@ -48,6 +47,6 @@ public class ProjectCategorySaveServlet extends HttpServlet {
 				} catch(NullPointerException | ServiceException e) {}
 			}
 		}
-		resp.sendRedirect(req.getContextPath() + "/project-category/list.html");
+		resp.sendRedirect(req.getContextPath() + "/admin/project/category/list.html");
 	}
 }
